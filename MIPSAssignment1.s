d43c2b6f3b5db8186f552dd3d21d00fd19ac8da7
theo7shelton <theo7shelton@gmail.com>
2017-11-12 04:27:38 -0500

    Initial Code
    1st commit has only the structure of an example mips program.

---
+++ MIPSAssignment1.s
@@ -0,0 +1,18 @@
+# MIPS Assignment 1
+# Programmed by Shelton Allen
+
+	.data
+
+			# Data declaration 
+
+	.text
+
+
+
+			# Assembly language instructions
+main:				# Begin main code
+	
+	li $v0, 10
+
+syscall
+	# Exit Program
