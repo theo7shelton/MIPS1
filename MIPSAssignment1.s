ec0bf0a0ed397f396627693e56465276b7affea2
theo7shelton <19628883+theo7shelton@users.noreply.github.com>
2017-11-13 00:33:33 -0500

    Update: Program takes input string
    Program now takes 8 keyboard character inputs, saves them and displays them.

--- MIPSAssignment1.s
+++ MIPSAssignment1.s
@@ -3,16 +3,26 @@
 
 	.data
 
-			# Data declaration 
+							# Data declaration 
+inputStr: 
+	.asciiz "Enter a string containing only hex"	
+buffer:	
+	.space 8
 
-	.text
-
-
-
-			# Assembly language instructions
-main:				# Begin main code
+	.text						# Assembly language instructions
+main:							# Begin main code
+	addi $t0, $zero, 0
+	li $v0, 4					# load print string call code
+	la $a0, inputStr				# load address of string to print
+	syscall						# print inputStr
 	
-	li $v0, 10
+	li $v0, 8					# load read string call code
+	la $a0, buffer					# load buffer address into $a0
+	li $a1, 9					# define amount of characters to be read
+	syscall						# system call for keyboard input
 
-syscall
-	# Exit Program
+	li $v0, 4					# load print string call code
+	syscall						# print buffer string
+
+	li $v0, 10					# Load exit code
+	syscall						# System call to exit
