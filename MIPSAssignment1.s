db809b995fb359cd9b73196470d2f382c03f2d0f
theo7shelton <19628883+theo7shelton@users.noreply.github.com>
2017-11-14 04:15:17 -0500

    Input Verification
    Function now verifies input by checking that the character entered is one of the following
      1. 0-9 
      2. a through f
      3. A through F
    Program also outputs error message is invalid character is entered.

--- MIPSAssignment1.s
+++ MIPSAssignment1.s
@@ -3,15 +3,23 @@
 
 	.data
 
-							# Data declaration 
+						# Data declaration 
 inputStr: 
-	.asciiz "Enter a string containing only hex"	
-buffer:	
-	.space 8
+	.asciiz "\nEnter a string containing only hex"	
 
-	.text						# Assembly language instructions
+invalid_inputStr:
+	.asciiz "\nThe string is invalid. Try again!"
+resultStr:
+	.asciiz "\nThe result is: "
+buffer:	
+	.space 9
+
+	.text
+
+
+
+						# Assembly language instructions
 main:							# Begin main code
-	addi $t0, $zero, 0
 	li $v0, 4					# load print string call code
 	la $a0, inputStr				# load address of string to print
 	syscall						# print inputStr
@@ -20,9 +28,53 @@ main:							# Begin main code
 	la $a0, buffer					# load buffer address into $a0
 	li $a1, 9					# define amount of characters to be read
 	syscall						# system call for keyboard input
+	
+	addi $s1, $zero, 0
+checkString: 
+	lb $t0, 0($a0)
+	beqz $t0, endFunc				# Check if current character is end of line character
+	li $t1, 10
+	beq $t0, $t1, endFunc				# End program if current character is newline character
 
+		# Check if in range of 0-9 ASCII
+	li $t1, 0x0000003A
+	slt $t2, $t0, $t1				# Sets $t2 if t0 <3Ah
+	li $t1, 0x0000002F				
+	slt $t3, $t1, $t0				# Sets $t3 if $t0 > 2Fh
+	and $s0, $t2, $t3				# Sets $s0 if byte is in range of 0-9 ASCII 
+	add $s1, $s1, $s0			
+		# Check if byte has code for ASCII a-f 
+	li $t1, 0x00000067
+	slt $t2, $t0, $t1				# Sets $t2 if t0 < 67
+	li $t1, 0x00000060				
+	slt $t3, $t1, $t0				# Sets $t3 if $t0 > 60
+	and $s0, $t2, $t3				# Sets $s0 if byte is in range of a-f ASCII 
+	add $s1, $s1, $s0
+
+		# Check if byte has code for ASCII A-F
+	li $t1, 0x00000047
+	slt $t2, $t0, $t1				# Sets $t2 if t0 < 47
+	li $t1, 0x00000040				
+	slt $t3, $t1, $t0				# Sets $t3 if $t0 > 40
+	and $s0, $t2, $t3				# Sets $s0 if byte is in range of A-F ASCII 
+	add $s1, $s1, $s0
+
+	beqz $s1, invalid
+	addi $a0, $a0, 1
+	li $s1, 0
+	li $s0, 0
+	j checkString
+invalid: 
 	li $v0, 4					# load print string call code
+	la $a0, invalid_inputStr			# Print error message
 	syscall						# print buffer string
 
-	li $v0, 10					# Load exit code
-	syscall						# System call to exit
+endFunc:
+	li $v0, 4
+	la $a0, resultStr
+	syscall
+	li $v0, 10
+
+					# Load exit code
+	syscall
+						# System call to exit
